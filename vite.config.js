b48f25a8887545a0a46d403cce0e59b2adbdea9e
queennie72 <queennie72@gmail.com>
2026-05-23 21:17:54 +0900

    update: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/todo/',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001',
+    },
+  },
 })
