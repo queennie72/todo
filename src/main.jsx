df3fe4aa0d56dfc68596edcdac9d2f627afd1111
queennie72 <queennie72@gmail.com>
2026-05-23 21:17:56 +0900

    update: src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App'
+import { initStorage } from './lib/storage'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+initStorage().then(() => {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
